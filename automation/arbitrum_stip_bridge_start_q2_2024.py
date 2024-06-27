8b7d6c02f4d00dda18383604afe1c7295e8697fc
Tritium-VLK <no@spam.com>
2024-06-27 15:46:08 +0200

    reallocate away from GYD pools which are currently being distributed to by gyro.

--- automation/arbitrum_stip_bridge_start_q2_2024.py
+++ automation/arbitrum_stip_bridge_start_q2_2024.py
@@ -104,19 +104,17 @@ ACTIVE_POOLS_AND_OVERRIDES = [
         "pool_id": "0x7967fa58b9501600d96bd843173b9334983ee6e600020000000000000000056e",
         "meta": {"symbol": "wstETH-wETH-ECLP"},
         "fixedBoost": 2,
-        "fixedEmissions": 8000,
+        "fixedEmissions": 10000,
     },
     {
         "pool_id": "0x7272163a931dac5bbe1cb5fefaf959bb65f7346f000200000000000000000549",
         "meta": {"symbol": "GYD-AUSDT-ECLP"},
         "fixedBoost": 1.75,
-        "fixedEmissions": 2000,
     },
     {
         "pool_id": "0x6e822c64c00393b2078f2a5bb75c575ab505b55c000200000000000000000548",
         "meta": {"symbol": "GYD-AUSDC-ECLP"},
         "fixedBoost": 1.75,
-        "fixedEmissions": 2000,
     },
     {
         "pool_id": "0x46472cba35e6800012aa9fcc7939ff07478c473e00020000000000000000056c",
@@ -127,11 +125,13 @@ ACTIVE_POOLS_AND_OVERRIDES = [
         "pool_id": "0x5b89dc91e5a4dc6d4ab0d970af6a7f981971a443000000000000000000000572",
         "meta": {"symbol": "instETH-wstETH"},
         "fixedBoost": 1.5,
+        "fixedEmissions": 1000,
     },
     {
         "pool_id": "0xb8cb384e65096386c1edaaf784e842c957fa3645000000000000000000000571",
         "meta": {"symbol": "inETH-wstETH"},
         "fixedBoost": 1.5,
+        "fixedEmissions": 1000,
     },
 ]
 
